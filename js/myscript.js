8f7edaf0ca254ccc0e1f8c5f1bf6d9e3eeb36495
Anna <rosalenanna@gmail.com>
2020-11-09 15:51:27 +0100

    stampa seconda parte

--- js/myscript.js
+++ js/myscript.js
@@ -4,10 +4,10 @@
 var studente = {
   "nome": "Anna",
   "cognome": "Rosalen",
-  "età": "26"
+  "età": 26
 }
 
-console.log("oggetto", studente);
+// console.log("oggetto", studente);
 
 // Stampare a schermo attraverso il for in tutte le proprietà.
 for (var key in studente){
@@ -18,7 +18,28 @@ for (var key in studente){
 // SECONDA PARTE*************
 // - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 
+var studenti =
+ [
+   {
+     "nome": "Marco",
+     "cognome" : "Verdi",
+     "età": 25
+   },
+   {
+     "nome": "Filippo",
+     "cognome" : "Rossi",
+     "età": 20
+   },
+   {
+     "nome": "Giorgia",
+     "cognome" : "Bianco",
+     "età": 25
+   }
+ ];
 
+ for(var key in studenti){
+   console.log(studenti[key].nome, studenti[key].cognome);
+ }
 
 
 
