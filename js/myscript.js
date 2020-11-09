0abfb763dc5779c250ca8770ca0dcb67810568a7
Anna <rosalenanna@gmail.com>
2020-11-09 15:34:22 +0100

    stampa prima parte

---
+++ js/myscript.js
@@ -0,0 +1,25 @@
+// PRIMA PARTE*************
+// - Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
+
+var studente = {
+  "nome": "Anna",
+  "cognome": "Rosalen",
+  "età": "26"
+}
+
+console.log("oggetto", studente);
+
+// Stampare a schermo attraverso il for in tutte le proprietà.
+for (var key in studente){
+  console.log(studente[key]);
+}
+
+
+// SECONDA PARTE*************
+// - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+
+
+
+
+// - Dare la possibilità all’utente attraverso 3 prompt di
+// aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
